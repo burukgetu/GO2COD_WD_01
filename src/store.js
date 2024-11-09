37b54f3cbb9c42a3ce915cb0e42d4676d19585ba
burukgetu <burukgetu000@gmail.com>
2024-11-09 15:45:54 -0300

    added to persist in local storage

--- src/store.js
+++ src/store.js
@@ -1,8 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import taskReducer from './reducers/taskSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
-export const store = configureStore({
+const persistConfig = {
+    key: 'root', // Key for local storage
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, taskReducer);
+
+const store = configureStore({
     reducer: {
-        tasks: taskReducer,
-    }
-})
+      tasks: persistedReducer,
+    },
+});
+  
+export const persistor = persistStore(store);
+export default store;
